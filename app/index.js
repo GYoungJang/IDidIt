8c3b6e9b653dc4e9ca09eac84162a068b9ec736c
GeunYoung Jang <zzbtang@gmail.com>
2022-12-26 05:44:40 +0900

    221226_swagger

--- app/index.js
+++ app/index.js
@@ -1,42 +1,43 @@
-const express = require("express");
+const express = require('express');
 const app = (module.exports = express());
-const bodyParser = require("body-parser");
-const dotenv = require("dotenv");
-const logger = require("morgan");
-const path = require("path");
-
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const logger = require('morgan');
+const path = require('path');
+const { swaggerUi, specs } = require('./swagger/swagger');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 dotenv.config();
 
-app.set("views", "./src/views/ejs");
-app.set("view engine", "ejs");
+app.set('views', './src/views/ejs');
+app.set('view engine', 'ejs');
 // app.set("views", path.join(__dirname, "views"));
 // app.use(express.static(path.join(__dirname, "static")));
 // app.set("static", path.join(__dirname, "static"));
-app.use(express.static("./src/static"));
+app.use(express.static('./src/static'));
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
 app.use(bodyParser.json());
-app.use(logger("dev"));
+app.use(logger('dev'));
 
-const routerUser = require("./src/routes/user");
-const routerMemo = require("./src/routes/memo");
-const routerCalendar = require("./src/routes/calendar");
-const { sequelize } = require("./src/model");
-
+const routerUser = require('./src/routes/user');
+const routerMemo = require('./src/routes/memo');
+const routerCalendar = require('./src/routes/calendar');
+const { sequelize } = require('./src/model');
+``;
 sequelize
   .sync({ force: false })
   .then(() => {
-    console.log("데이터베이스 연결 성공");
+    console.log('데이터베이스 연결 성공');
   })
   .catch((err) => {
     console.error(err);
   });
 
-app.use("/", routerUser);
-app.use("/memo", routerMemo);
-app.use("/calendar", routerCalendar);
+app.use('/', routerUser);
+app.use('/memo', routerMemo);
+app.use('/calendar', routerCalendar);
 
 // 404 예외처리
 app.use(function (req, res, next) {
-  res.status(404).render("404");
+  res.status(404).render('404');
 });
